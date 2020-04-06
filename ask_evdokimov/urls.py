05ce5babc8c919cc9eb84de19aff3600f093b984
Arugaf <arug113@gmail.com>
2020-04-06 10:47:40 +0300

    style fixes, admin, login for navbar, view for questions

--- ask_evdokimov/urls.py
+++ ask_evdokimov/urls.py
@@ -3,10 +3,11 @@ from ask_evdokimov import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('1', views.question, name="question"),
-    path('tag', views.tag, name="tag"),
+    path('question/<question_id>', views.question, name="question"),
+    path('tag/<tag_name>', views.tag, name="tag"),
     path('ask', views.ask_question, name="ask_question"),
     path('login', views.login, name="login"),
     path('reg', views.registration, name="registration"),
-    path('user', views.user_settings, name="user")
+    path('user', views.user_settings, name="user"),
+    path('hot', views.hot, name="hot")
 ]
--- ask_evdokimov/views.py
+++ ask_evdokimov/views.py
@@ -1,18 +1,45 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'index.html')
+def index(request, is_hot=False):
+    questions = [{
+        'title': 'question' + str(i),
+        'id': i,
+        'text': '''Ambitioni dedisse scripsisse iudicaretur. Cras mattis iudicium purus sit amet fermentum. Donec 
+            sed odio operae, eu vulputate felis rhoncus. Praeterea iter est quasdam res quas ex communi. At nos hinc 
+            posthac, sitientis piros Afros. Petierunt uti sibi concilium totius Galliae in diem certam indicere. Cras 
+            mattis iudicium purus sit amet fermentum. ''' + str(i),
+        'tag': ['tag1', 'tag2']
+    } for i in range(0, 20)]
+
+    context = {'questions': questions}
+    if is_hot is True:
+        context['hot'] = True
+
+    return render(request, 'index.html', context)
 
 
-def question(request):
+def hot(request):
+    return index(request, True)
+
+
+def question(request, question_id):
     return render(request, 'question.html')
 
 
-def tag(request):
-    return render(request, 'tag.html')
+def tag(request, tag_name):
+    context = {}
+    questions = []
+    for i in range(1, 5):
+        questions.append(
+            {'title': 'question' + str(i), 'id': i, 'text': '''mnogo texta lol''', 'tag': ['tag1', 'tag2']})
+    context['questions'] = questions
+    context['tag'] = tag_name
+    return render(request, 'index.html', context)
 
 
+@login_required
 def ask_question(request):
     return render(request, 'ask_question.html')
 
